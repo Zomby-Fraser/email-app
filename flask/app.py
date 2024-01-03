e53873460b7b88946000f90ba45cf600a32e305a
zomby-fraser <albert@mgrestorations.com>
2024-01-03 02:13:43 -0500

    test

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, session
+from dotenv import load_dotenv
+import mysql.connector
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+	user = request.form.get('user')
+	password = request.form.get('password')
+
+	if not user or not password:
+		return jsonify({'error': 'Missing username or password'}), 400
+
+	hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+	try:
+		conn = database.new_conn()
+
+		query = '''SELECT 
+            u.id AS user_id,
+            u.username AS username,
+            ur.users_role_id AS role_id
+        FROM Users u
+        INNER JOIN UserRoles ur ON ur.users_id = u.id
+        WHERE u.username = %s AND u.password = %s'''
+		user = database.pull(conn, query, (username, hashed_password))
+		conn.close()
+
+		user = user[0]
+
+		if user:
+			session['role_id'] = user['role_id']
+			session['id'] = user['user_id']
+			session['username'] = user['username']
+			return jsonify({'message': 'Login successful'}), 200
+		else:
+			return jsonify({'error': 'Invalid credentials'}), 401
+
+	except mysql.connector.Error as err:
+		return jsonify({'error': str(err)}), 500
+	except Exception as err:
+		return jsonify({'error': str(err)}), 500
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
---
+++ flask/static/js/index.js
@@ -0,0 +1,19 @@
+async function login() {
+    const user = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch(`/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `user=${user}&password=${password}`
+    });
+    if (response.ok) {
+        console.log('Update Successful.');
+    }
+    else {
+        console.error('Update failed:', response.status, response.statusText);
+    }
+}
+
